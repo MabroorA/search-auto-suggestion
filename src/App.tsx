0c2fa5ced9e28b21298d7a539a65e1d6a3d9cf85
Mabroor <mabroorahmed18@gmail.com>
2024-07-19 17:04:13 +0100

    Display of each suggestion result can now be turned off and on

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,9 @@ function App() {
   );
   const [collectionResults, setCollectionResults] = useState<Collection[]>([]);
   const [productResults, setProductResults] = useState<Product[]>([]);
+  const [isSuggestionsOpen, setIsSuggestionsOpen] = useState<boolean>(true);
+  const [isCollectionsOpen, setIsCollectionsOpen] = useState<boolean>(true);
+  const [isProductsOpen, setIsProductsOpen] = useState<boolean>(true);
 
   const fetchSuggestionResults = async () => {
     try {
@@ -105,16 +108,64 @@ function App() {
       )
     );
   };
-  // const handleCloseSuggestionterm = {
-  //   // To be implemented
-  // }
+  const handleToggleSection = (section: string) => {
+    switch (section) {
+      case "suggestions":
+        setIsSuggestionsOpen(!isSuggestionsOpen);
+        break;
+      case "collections":
+        setIsCollectionsOpen(!isCollectionsOpen);
+        break;
+      case "products":
+        setIsProductsOpen(!isProductsOpen);
+        break;
+      default:
+        break;
+    }
+  };
   return (
-    <div className="flex flex-col justify-center items-center mx-auto p-5 w-2/5">
+    <div className="flex flex-col justify-center  mx-auto p-5 w-2/5">
       {/* Search Section */}
       <div className="flex flex-row justify-center items-center space-x-2">
         <div className="text-black font-bold">Search</div>
         <div>
           <input className="rounded-lg border-2" onChange={handleInputChange} />
+        </div>
+        {/* turn of section */}
+        <div className="flex flex-row space-x-2 max-w-lg bg-white rounded-lg ">
+          <div className="text-black font-bold text-lg mb-2">
+            Turn Off Block
+          </div>
+          <div className="flex flex-row space-x-2">
+            <button
+              className={`border rounded-md ${
+                isSuggestionsOpen
+                  ? "bg-black text-white"
+                  : "bg-red-300 text-white"
+              } p-1`}
+              onClick={() => handleToggleSection("suggestions")}
+            >
+              Suggestions
+            </button>
+            <button
+              className={`border rounded-md ${
+                isCollectionsOpen
+                  ? "bg-black text-white"
+                  : "bg-red-300 text-white"
+              } p-1`}
+              onClick={() => handleToggleSection("collections")}
+            >
+              Collections
+            </button>
+            <button
+              className={`border rounded-md ${
+                isProductsOpen ? "bg-black text-white" : "bg-red-300 text-white"
+              } p-1`}
+              onClick={() => handleToggleSection("products")}
+            >
+              Products
+            </button>
+          </div>
         </div>
       </div>
 
@@ -122,63 +173,86 @@ function App() {
       {query.trim() && dataResult && (
         <div className="border border-gray-300 rounded-lg mt-4">
           {/* Suggestions */}
-          <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
-            <div className="font-medium w-full">SUGGESTIONS</div>
-            <div>close</div>
-          </div>
-          {suggestionResults.map((suggestion) => (
-            <div
-              key={suggestion.term}
-              className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
-            >
-              <a href={suggestion.url} className="text-black-500">
-                {boldQueryInTerm(suggestion.term, query)}
-              </a>
+          {isSuggestionsOpen && (
+            <div>
+              <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
+                <div className="font-medium w-full">SUGGESTIONS</div>
+                <button onClick={() => setIsSuggestionsOpen(false)}>
+                  Close
+                </button>
+              </div>
+              {suggestionResults.length > 0
+                ? suggestionResults.map((suggestion) => (
+                    <div
+                      key={suggestion.term}
+                      className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
+                    >
+                      <a href={suggestion.url} className="text-black-500">
+                        {boldQueryInTerm(suggestion.term, query)}
+                      </a>
+                    </div>
+                  ))
+                : ""}
             </div>
-          ))}
+          )}
 
           {/* Collections */}
-          <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
-            <div className="font-medium w-full">COLLECTIONS</div>
-            <div>close</div>
-          </div>
-          {collectionResults.map((collection) => (
-            <div
-              key={collection.id}
-              className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
-            >
-              <a href={collection.url} className="text-black-500">
-                {boldQueryInTerm(collection.title, query)}
-              </a>
-            </div>
-          ))}
-
-          {/* Products */}
-          <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
-            <div className="font-medium w-full">PRODUCTS</div>
-            <div>close</div>
-          </div>
-          {productResults.map((product) => (
-            <div
-              key={product.id}
-              className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
-            >
-              <div className="flex flex-row justify-start w-full">
-                <div className="w-1/4">
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    className="text-black-500"
-                  />
-                </div>
-                <div className="flex flex-col ml-2">
-                  <div className="text-lg font-semibold">{product.title}</div>
-                  <div>{product.brand}</div>
-                  <div className="font-black">£{product.price}</div>
-                </div>
+          {isCollectionsOpen && (
+            <div>
+              <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
+                <div className="font-medium w-full">COLLECTIONS</div>
+                <button onClick={() => setIsCollectionsOpen(false)}>
+                  Close
+                </button>
               </div>
+              {collectionResults.length > 0
+                ? collectionResults.map((collection) => (
+                    <div
+                      key={collection.id}
+                      className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
+                    >
+                      <a href={collection.url} className="text-black-500">
+                        {boldQueryInTerm(collection.title, query)}
+                      </a>
+                    </div>
+                  ))
+                : ""}
             </div>
-          ))}
+          )}
+          {/* Products */}
+          {isProductsOpen && (
+            <div>
+              <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
+                <div className="font-medium w-full">PRODUCTS</div>
+                <button onClick={() => setIsProductsOpen(false)}>Close</button>
+              </div>
+              {productResults.length > 0
+                ? productResults.map((product) => (
+                    <div
+                      key={product.id}
+                      className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
+                    >
+                      <div className="flex flex-row justify-start w-full">
+                        <div className="w-1/4">
+                          <img
+                            src={product.image}
+                            alt={product.title}
+                            className="text-black-500"
+                          />
+                        </div>
+                        <div className="flex flex-col ml-2">
+                          <div className="text-lg font-semibold">
+                            {product.title}
+                          </div>
+                          <div>{product.brand}</div>
+                          <div className="font-black">£{product.price}</div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                : ""}
+            </div>
+          )}
         </div>
       )}
     </div>
@@ -186,6 +260,3 @@ function App() {
 }
 
 export default App;
-
-// Suggestion result
-//  Add Filter feature for limit 
