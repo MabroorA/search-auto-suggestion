e7bc9ca026eb6778db2ca1e892c87cbfd91e91a6
Mabroor <mabroorahmed18@gmail.com>
2024-07-19 15:34:21 +0100

    UI layout add for suggestions,collection and products card added

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,11 @@ interface Data {
 function App() {
   const [query, setQuery] = useState<string>("");
   const [dataResult, setDataResult] = useState<Data | null>(null);
-  const [suggestionResults, setSuggestionResults] = useState<SuggestionTerm[]>([]);
+  const [suggestionResults, setSuggestionResults] = useState<SuggestionTerm[]>(
+    []
+  );
   const [collectionResults, setCollectionResults] = useState<Collection[]>([]);
   const [productResults, setProductResults] = useState<Product[]>([]);
-  
 
   const fetchSuggestionResults = async (query: string) => {
     try {
@@ -45,14 +46,14 @@ function App() {
         throw new Error("Network response was not ok");
       }
       const data: Data = await res.json();
-      
+
       setDataResult(data);
-      console.log(data)
+      console.log(data);
       // const suggestionsResult: SuggestionTerm[] = await res.json()
       setSuggestionResults(data.suggestion_terms);
       setCollectionResults(data.collections);
       setProductResults(data.products);
-      console.log(suggestionResults)
+      console.log(suggestionResults);
     } catch (error) {
       console.error("Error Fetching Suggestion Results", error);
     }
@@ -84,29 +85,71 @@ function App() {
   //   // To be implemented
   // }
   return (
-    <div className="  flex flex-col justify-center align-middle p-5  ">
+    <div className="flex flex-col justify-center items-center mx-auto p-5 w-2/5">
       {/* Search Section */}
-      <div className="flex flex-row justify-center align-middle space-x-2">
+      <div className="flex flex-row justify-center items-center space-x-2">
         <div className="text-black font-bold">Search</div>
         <input className="rounded-lg border-2" onChange={handleInputChange} />
       </div>
 
-      {/* Results Section  */}
-      {query.trim() && suggestionResults.length > 0 && (
-        <div className="border border-gray-300 rounded-lg mt-4 ">
+      {/* Results Section */}
+      {query.trim() && dataResult && (
+        <div className="border border-gray-300 rounded-lg mt-4">
+          {/* Suggestions */}
           <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
-            <div className=" font-medium  w-full">SUGGESTIONS</div>
-            <div className="">close</div>
+            <div className="font-medium w-full">SUGGESTIONS</div>
+            <div>close</div>
           </div>
           {suggestionResults.map((suggestion) => (
-            <div className="p-2">
-              <div
-                key={suggestion.term}
-                className="mb-2 hover:bg-blue-200 hover:rounded-md"
-              >
-                <a href={suggestion.url} className="text-black-500 ">
-                  {boldQueryInTerm(suggestion.term, query)}
-                </a>
+            <div
+              key={suggestion.term}
+              className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
+            >
+              <a href={suggestion.url} className="text-black-500">
+                {boldQueryInTerm(suggestion.term, query)}
+              </a>
+            </div>
+          ))}
+
+          {/* Collections */}
+          <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
+            <div className="font-medium w-full">COLLECTIONS</div>
+            <div>close</div>
+          </div>
+          {collectionResults.map((collection) => (
+            <div
+              key={collection.id}
+              className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
+            >
+              <a href={collection.url} className="text-black-500">
+                {boldQueryInTerm(collection.title, query)}
+              </a>
+            </div>
+          ))}
+
+          {/* Products */}
+          <div className="flex flex-row justify-between bg-slate-300 text-gray-700 p-2">
+            <div className="font-medium w-full">PRODUCTS</div>
+            <div>close</div>
+          </div>
+          {productResults.map((product) => (
+            <div
+              key={product.id}
+              className="p-2 mb-2 hover:bg-blue-200 hover:rounded-md"
+            >
+              <div className="flex flex-row justify-start w-full">
+                <div className="w-1/4">
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="text-black-500"
+                  />
+                </div>
+                <div className="flex flex-col ml-2">
+                  <div className="text-lg font-semibold">{product.title}</div>
+                  <div>{product.brand}</div>
+                  <div className="font-black">£{product.price}</div>
+                </div>
               </div>
             </div>
           ))}
